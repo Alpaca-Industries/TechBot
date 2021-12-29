fc545749e4b319eed8849486f827a63a244bad77
Spen <69215413+DevSpen@users.noreply.github.com>
2021-12-29 16:52:51 -0500

    this is probably broken

---
+++ src/commands/economy/income/daily.ts
@@ -0,0 +1,37 @@
+import type { Args, CommandContext, CommandOptions } from '@sapphire/framework';
+import { Message, MessageEmbed } from 'discord.js';
+
+import { Command } from '@sapphire/framework';
+import { ApplyOptions } from '@sapphire/decorators';
+import { fetchUser } from '../../../helpers/dbHelper';
+
+@ApplyOptions<CommandOptions>({
+	name: 'daily',
+	description: 'Get those yummy pepe coins, I know you want them.',
+    cooldownDelay: 86_400_000
+})
+export default class DailyCommand extends Command {
+	async messageRun(
+		message: Message<boolean>,
+		args: Args,
+		context: CommandContext
+	): Promise<unknown> {
+		const embed = new MessageEmbed();
+
+		const moneyEarned = Math.round(
+			Math.random() * (3000 - 750) + 750
+		);
+
+		fetchUser(message.author).then(user => {
+			user.wallet += moneyEarned;
+			user.save();
+		});
+
+		embed
+			.setTitle('Daily Coins :D')
+			.setDescription(`Ayyy! You earned **$${moneyEarned}**, see ya tommorow.`)
+			.setColor('BLUE')
+
+		return message.channel.send({ embeds: [embed] });
+	}
+}
