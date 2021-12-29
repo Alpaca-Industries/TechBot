4a5f2b01f304e6addc09fc04d9729f52d725437e
Spen <69215413+DevSpen@users.noreply.github.com>
2021-12-29 17:53:57 -0500

    .

--- src/listeners/commandDenied.ts
+++ src/listeners/commandDenied.ts
@@ -4,7 +4,7 @@ import { Listener } from '@sapphire/framework';
 export class CommandDeniedListener extends Listener {
   public run(error: UserError & { context: { remaining?: number } }, { message }: CommandDeniedPayload) {
     if (error.name == "Cooldown") {   
-        message.channel.send(`You must wait <t:${parseInt((new Date(error.context.remaining).getTime() / 1000).toFixed(0))}:f> (${parseInt((new Date(error.context.remaining).getTime() / 1000).toFixed(0))}:R>) before using that command again.`);
+        message.channel.send(`You must wait <t:${parseInt((new Date(error.context.remaining).getTime() / 1000).toFixed(0))}:f> (<t:${parseInt((new Date(error.context.remaining).getTime() / 1000).toFixed(0))}:R>) before using that command again.`);
     } else {
         message.channel.send(error.message);
     }
