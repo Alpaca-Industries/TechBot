48e7aa4d4da5eeae1ad0e96a50aa7d2fdacaba4b
Scott Gould <greysilly7@gmail.com>
2021-12-29 17:24:38 -0500

    Errors go bye bye

--- src/listeners/commandDenied.ts
+++ src/listeners/commandDenied.ts
@@ -2,7 +2,7 @@ import type { UserError, CommandDeniedPayload } from '@sapphire/framework';
 import { Listener } from '@sapphire/framework';
 
 export class CommandDeniedListener extends Listener {
-  public run(error: UserError, { message }: CommandDeniedPayload) {
+  public run(error: UserError & { context: { remaining?: number } }, { message }: CommandDeniedPayload) {
     if (error.name == "Cooldown") {   
         message.channel.send(`You must wait <t:${Math.floor(error.context.remaining / 1000)}:f> (<t:${Math.floor(error.context.remaining / 1000)}:R>) before using that command again.`);
     } else {
