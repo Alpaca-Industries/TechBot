0c883e35d5f4de665394cfd6685c9c2eff610716
Scott Gould <greysilly7@gmail.com>
2021-12-29 20:31:12 -0500

    Fix Spen's Coinflip command

--- src/commands/fun/coinflip.ts
+++ src/commands/fun/coinflip.ts
@@ -1,5 +1,6 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { Args, Command, CommandContext, CommandOptions } from '@sapphire/framework';
+import type { Message } from 'discord.js';
 
 @ApplyOptions<CommandOptions>({
 	name: 'coinflip',
@@ -12,6 +13,7 @@ export class PingCommand extends Command {
 		context: CommandContext
 	): Promise<unknown> {
         if (Math.floor(Math.random() * 2) === 1) return message.reply("Heads")
-        if (Math.floor(Math.random() * 2) === 1) return message.reply("Tails")        
+        if (Math.floor(Math.random() * 2) === 1) return message.reply("Tails")
+		return;
 	}
 }
