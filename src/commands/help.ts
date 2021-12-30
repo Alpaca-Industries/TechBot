83900235b8cef42e145252f980f595d43e1c4ff2
Scott Gould <greysilly7@gmail.com>
2021-12-30 13:42:03 -0500

    SPen deleted stuff

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -12,29 +12,25 @@ import { PaginatedMessage } from '@sapphire/discord.js-utilities';
 })
 export default class helpCommand extends Command {
 	async messageRun(message: Message<boolean>, args: Args, context: CommandContext): Promise<unknown> {
+		const specifiedCommand = await args.pickResult('string');
 		// List All Commands Registered In Sapphire
 		const commands = this.container.stores.get('commands');
 		const categories = commands.categories;
 		const paginatedMessage = new PaginatedMessage({ template: new MessageEmbed().setTitle('Help') });
-		/*
-		const embed = new MessageEmbed();
-		for (const command of commands) {
-			const { name, description, fullCategory } = command;
-			console.log(fullCategory);
-			embed.addField(name || 'No name', description || 'No Description', true);
+
+		if (specifiedCommand.success === true) {
+			const command = commands.find(c => c.name === specifiedCommand.value);
+			if (command === undefined) return message.reply('That command does not exist!');
+
+			paginatedMessage.addPageEmbed(new MessageEmbed()
+				.setTitle(`${command.name}`)
+				.setDescription(`${command.description}`)
+				.setColor('#20ce1f')
+			);
 		}
-		*/
 
 		for (const category of categories) {
 			const fields: { name: string; value: any }[] = [];
-			/*
-			commands.filter(c => String(c.fullCategory) === category || String(c.fullCategory[c.fullCategory.length]) === category[category.length]).forEach(command => {
-                fields.push({
-                    name: `${command.name}`,
-                    value: command.detailedDescription || command.description
-                });
-            });
-			*/
 
 			// Filter commands to categories and take intoacount sub categories
 			const filteredCommands = commands.filter(c => String(c.fullCategory) === category || String(c.fullCategory[c.fullCategory.length]) === category[category.length]);
