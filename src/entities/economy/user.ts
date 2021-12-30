590ae9693adbcdd9af980d99784c082cd8b40df7
Scott Gould <greysilly7@gmail.com>
2021-12-29 20:05:08 -0500

    Got off track

--- src/entities/economy/user.ts
+++ src/entities/economy/user.ts
@@ -19,4 +19,13 @@ export class User extends BaseEntity {
 
 	@OneToMany(type => Inventory, inventory => inventory.userId)
     inventory: Inventory[];
+
+	@Column()
+	gender: string;
+
+	@Column()
+	age: string;
+
+	@Column()
+	hobby: string;
 }
