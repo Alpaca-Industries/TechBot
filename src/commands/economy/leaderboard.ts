cf7f617693d86b02d859dfab623bae252b5abe54
Scott Gould <greysilly7@gmail.com>
2022-01-01 01:21:24 -0500

    feat(leaderboard): Flags for leaderboard to specify diffrent things

--- src/commands/economy/leaderboard.ts
+++ src/commands/economy/leaderboard.ts
@@ -7,110 +7,67 @@ import { User } from '../../entities/economy/user';
 
 @ApplyOptions<CommandOptions>({
 	name: 'leaderboard',
-	description: "Shows the global economy leaderboard.",
-	detailedDescription: 'leaderboard'
+	description: 'Shows the global economy leaderboard.',
+	detailedDescription: 'leaderboard',
+	flags: ['guildOnly', 'ownerOnly', 'bankOnly', 'overallMoney']
 })
 export default class LeaderboardCommand extends Command {
 	private numToEnglish(number: number): string {
-		const num = [
-			'zero',
-			'one',
-			'two',
-			'three',
-			'four',
-			'five',
-			'six',
-			'seven',
-			'eight',
-			'nine',
-			'ten',
-			'eleven',
-			'twelve',
-			'thirteen',
-			'fourteen',
-			'fifteen',
-			'sixteen',
-			'seventeen',
-			'eighteen',
-			'nineteen'
-		];
+		const num = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
 		if (number < 20) return num[number];
-		const tens = [
-			'twenty',
-			'thirty',
-			'forty',
-			'fifty',
-			'sixty',
-			'seventy',
-			'eighty',
-			'ninety'
-		];
+		const tens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
 		const digit = number % 10;
 		// 												Added strict if check here instead of ambiguous
-		if (number < 100)
-			return `${tens[~~(number / 10) - 2]}${
-				digit !== 0 ? '-' + num[digit] : ''
-			}`;
+		if (number < 100) return `${tens[~~(number / 10) - 2]}${digit !== 0 ? '-' + num[digit] : ''}`;
 		// Changed return types to string so its actually clear whats returned
-		if (number < 1000)
-			return `${num[~~(number / 100)]} hundred ${
-				number % 100 == 0 ? '' : ' ' + this.numToEnglish(number % 100)
-			}`;
-		return `${this.numToEnglish(~~(number / 1000))} thousand ${
-			number % 1000 != 0 ? ' ' + this.numToEnglish(number % 1000) : ''
-		}`;
+		if (number < 1000) return `${num[~~(number / 100)]} hundred ${number % 100 == 0 ? '' : ' ' + this.numToEnglish(number % 100)}`;
+		return `${this.numToEnglish(~~(number / 1000))} thousand ${number % 1000 != 0 ? ' ' + this.numToEnglish(number % 1000) : ''}`;
 	}
 
-	async messageRun(
-		message: Message<boolean>,
-		args: Args,
-		context: CommandContext
-	): Promise<unknown> {
-		const topUsers = await User.createQueryBuilder('user')
-			.orderBy('user.wallet', 'DESC')
-			.limit(10)
-			.getMany();
+	async messageRun(message: Message<boolean>, args: Args, context: CommandContext): Promise<unknown> {
+		const guildOnly = args.getFlags('guildOnly');
+		const walletOnly = args.getFlags('walletOnly');
+		const bankOnly = args.getFlags('bankOnly');
+		const overallMoney = args.getFlags('overallMoney');
+
+		if (guildOnly === true && walletOnly === true) {
+			return message.channel.send('Please Only Specify Either Bank or Wallet or Overalll');
+		}
+
+		const topUsers = await User.createQueryBuilder('user').orderBy('user.wallet', 'DESC').limit(10).getMany();
 		const leaderboardEmbed = new MessageEmbed();
 		const leaderboardData: string[] = [];
 
 		let counter = 1;
-		for (const user of topUsers.filter(user => user.wallet > 0)) {
-			const userInformation = await this.container.client.users.fetch(
-				user.id
-			);
+		for (const user of topUsers.filter((user) => {
+			const filterOutUsersBelow0Condition = user.wallet + user.bank > 0;
+			const guildOnlyCondition = guildOnly === true ? message.guild.members.fetch(user.id) : true;
+			return filterOutUsersBelow0Condition && guildOnlyCondition;
+		})) {
+			const userInformation = await this.container.client.users.fetch(user.id);
 
+			const valueForEmbed = () => {
+				if (overallMoney === true) return user.wallet + user.bank;
+				if (walletOnly === true) return user.wallet;
+				if (bankOnly === true) return user.bank;
+				return user.wallet;
+			};
 
 			switch (counter) {
 				// Removed unecceary {} around case statements
 				case 1:
 					// Made all lines single lines so its actually readable, for the love of god change your max line length
-					leaderboardData.push(
-						`:first_place: • ${userInformation.tag} - ${
-							user.wallet ? user.wallet.toLocaleString() : 0
-						}`
-					);
+					leaderboardData.push(`:first_place: • ${userInformation.tag} - ${valueForEmbed() ? valueForEmbed().toLocaleString() : 0}`);
 					break;
 				case 2:
-					leaderboardData.push(
-						`:second_place: • ${userInformation.tag} - ${
-							user.wallet ? user.wallet.toLocaleString() : 0
-						}`
-					);
+					leaderboardData.push(`:second_place: • ${userInformation.tag} - ${valueForEmbed() ? valueForEmbed().toLocaleString() : 0}`);
 					break;
 				case 3:
-					leaderboardData.push(
-						`:third_place: • ${userInformation.tag} - ${
-							user.wallet ? user.wallet.toLocaleString() : 0
-						}`
-					);
+					leaderboardData.push(`:third_place: • ${userInformation.tag} - ${valueForEmbed() ? valueForEmbed().toLocaleString() : 0}`);
 					break;
 				default:
-					leaderboardData.push(
-						`:${this.numToEnglish(counter)}: • ${
-							userInformation.tag
-						} - ${user.wallet ? user.wallet.toLocaleString() : 0}`
-					);
+					leaderboardData.push(`:${this.numToEnglish(counter)}: • ${userInformation.tag} - ${valueForEmbed() ? valueForEmbed().toLocaleString() : 0}`);
 			}
 			counter++;
 		}
