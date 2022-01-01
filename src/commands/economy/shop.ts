9d81dfe8e41e0210fd10766b0b6da702bd681fba
Scott Gould <greysilly7@gmail.com>
2021-12-31 22:50:51 -0500

    feat(shop): Added ability to specify an item

--- src/commands/economy/shop.ts
+++ src/commands/economy/shop.ts
@@ -11,14 +11,16 @@ import { Item } from '../../entities/economy/item';
 	detailedDescription: 'shop'
 })
 export default class ShopCommand extends Command {
-	async messageRun(
-		message: Message<boolean>,
-		args: Args,
-		context: CommandContext
-	): Promise<unknown> {
-		const items = await Item.createQueryBuilder('item')
-			.orderBy('item.price', 'ASC')
-			.getMany();
+	async messageRun(message: Message<boolean>, args: Args, context: CommandContext): Promise<unknown> {
+		const specificItem = await args.pickResult('string');
+		if (specificItem.success === true) {
+			const item = await Item.findOne({ where: { name: specificItem.value } });
+			if (item !== undefined) {
+				const embed = new MessageEmbed().setTitle(item.name).setDescription(`Price: ${item.price}`).setColor(0x00ff00);
+				return message.channel.send({ embeds: [embed] });
+			}
+		}
+		const items = await Item.createQueryBuilder('item').orderBy('item.price', 'ASC').getMany();
 
 		const embed = new MessageEmbed();
 
