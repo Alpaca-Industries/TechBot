609e16d7081e241c62fda7418da46c9f9de5e204
Scott Gould <greysilly7@gmail.com>
2021-12-31 23:57:14 -0500

    Changed output of ping command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -8,17 +8,9 @@ import { Message } from 'discord.js';
 	detailedDescription: 'ping'
 })
 export class PingCommand extends Command {
-	async messageRun(
-		message: Message<boolean>,
-		args: Args,
-		context: CommandContext
-	): Promise<unknown> {
+	async messageRun(message: Message<boolean>, args: Args, context: CommandContext): Promise<unknown> {
 		const msg = await message.channel.send('Pinging...');
-		const content = `Pong from JavaScript! Bot Latency ${Math.round(
-			this.container.client.ws.ping
-		)}ms. API Latency ${
-			msg.createdTimestamp - message.createdTimestamp
-		}ms.`;
+		const content = `Pong! Bot Latency ${Math.round(this.container.client.ws.ping)}ms. API Latency ${(msg.editedTimestamp || msg.createdTimestamp) - (message.editedTimestamp || message.createdTimestamp)}ms.`;
 
 		return msg.edit(content);
 	}
