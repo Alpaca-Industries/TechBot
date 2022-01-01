28e8a577bb519b604665ce345539fc5e2ad07332
Scott Gould <greysilly7@gmail.com>
2022-01-01 00:59:03 -0500

    Arrow Functions On Top

--- src/helpers/logging.ts
+++ src/helpers/logging.ts
@@ -1,7 +1,7 @@
 import type { Message } from 'discord.js';
 import { MessageEmbed } from 'discord.js';
 
-export function sendError(message: Message, title: string, description: string) {
+export const sendError = (message: Message, title: string, description: string) => {
 	const errorEmbed = new MessageEmbed().setTitle(`Error: ${title}`).setDescription(description).setColor('RED');
 	message.channel.send({ embeds: [errorEmbed] });
-}
+};
