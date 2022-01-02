b36d81286128e3560f0ed82496294ca2c9aa3c39
Scott Gould <greysilly7@gmail.com>
2022-01-01 22:53:07 -0500

    Added ability to mention bot to get useful information

---
+++ src/listeners/message.ts
@@ -0,0 +1,21 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import type { ListenerOptions } from '@sapphire/framework';
+import { Listener } from '@sapphire/framework';
+import { Message, MessageEmbed } from 'discord.js';
+
+@ApplyOptions<ListenerOptions>({
+	event: 'messageCreate'
+})
+export default class messageListener extends Listener {
+	run(message: Message<boolean>): unknown {
+		if (message.author.bot) return;
+		if (message.content.startsWith(`<@!${message.client.user.id}>`)) {
+			const embed = new MessageEmbed()
+				.setTitle('Hello!')
+				.setDescription(`I am a bot created by Greysilly7#8813, Spen#0999, and Haider#8515.\n\nI am a bot that can be used to manage your server\'s economy.\n\nTo get started, use the command \`help\` to see a list of commands. \n\n My Prefix is **${message.client.fetchPrefix(message)}**`)
+				.setColor(0x00ff00);
+			return message.channel.send({ embeds: [embed] });
+		}
+		return;
+	}
+}
