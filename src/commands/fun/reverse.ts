40ee3c5841a6d88a8edfd10eb317aef5ee017af7
Spen <69215413+DevSpen@users.noreply.github.com>
2022-01-07 17:36:41 -0500

    feat(reverse): added reverse command

---
+++ src/commands/fun/reverse.ts
@@ -0,0 +1,20 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { Args, Command, CommandContext, CommandOptions } from '@sapphire/framework';
+import type { Message } from 'discord.js';
+
+@ApplyOptions<CommandOptions>({
+	name: 'reverse',
+	description: 'Reverse your text.',
+	detailedDescription: 'reverse [string]'
+})
+export class ReverseCommand extends Command {
+	async messageRun(
+		message: Message<boolean>,
+		args: Args,
+		context: CommandContext
+	): Promise<unknown> {
+        const arg = await args.restResult('string');
+		if (!arg.success) return message.reply('Please specify a string to reverse!');
+		return message.channel.send(arg.split(' ').reverse().join(' '));
+	}
+}
