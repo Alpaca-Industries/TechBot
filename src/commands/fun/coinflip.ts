47c64ab3e5325cec272b076c19c407aad3020d0a
Scott Gould <greysilly7@gmail.com>
2022-01-09 21:17:23 -0500

    Fix coinflip

--- src/commands/fun/coinflip.ts
+++ src/commands/fun/coinflip.ts
@@ -10,16 +10,13 @@ import type { CommandInteraction, Message } from 'discord.js';
 })
 export class CoinFlipCommand extends Command {
 	async messageRun(message: Message<boolean>, args: Args) {
-		if (Math.floor(Math.random() * 2) === 1) return message.reply('Heads');
-		if (Math.floor(Math.random() * 2) === 1) return message.reply('Tails');
-		// @ts-ignore
-		return;
+		if (Math.random() > 0.5) return message.reply('Heads');
+		else return message.reply('Tails');
 	}
 
 	async chatInputRun(interaction: CommandInteraction) {
-		if (Math.floor(Math.random() * 2) === 1) return interaction.reply('Heads');
-		if (Math.floor(Math.random() * 2) === 1) return interaction.reply('Tails');
-		return;
+		if (Math.random() > 0.5) return interaction.reply('Heads');
+		else return interaction.reply('Tails');
 	}
 
 	registerApplicationCommands(registry: ApplicationCommandRegistry) {
