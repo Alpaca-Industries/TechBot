80fec6a75bf043b4ed0382f301f64f17d40da81c
DevSpen <spenmusic6@gmail.com>
2022-01-13 20:09:06 -0500

    feat(parseAmount): Added parseAmount() helper

---
+++ src/helpers/parseAmount.ts
@@ -0,0 +1,19 @@
+export function parseAmount(amount: any, user: any, useWallet: boolean = true) {
+	amount = amount.toLowerCase();
+	if (useWallet) {
+		if (amount === 'all') return user.wallet;
+		if (amount === 'half') return Math.trunc(user.wallet / 2);
+		if (amount === 'third') return Math.trunc(user.wallet / 3);
+		if (amount === 'quarter' || amount === 'fourth') return Math.trunc(user.wallet / 4);
+	} else {
+		if (amount === 'all') return user.bank;
+		if (amount === 'half') return Math.trunc(user.bank / 2);
+		if (amount === 'third') return Math.trunc(user.bank / 3);
+		if (amount === 'quarter' || amount === 'fourth') return Math.trunc(user.bank / 4);
+	}
+
+	if (/^([+])?(\d+)\.?(\d*)[eE]([+]?\d+)$/.test(amount)) return Number(amount);
+	if (isNaN(amount)) return parseInt(amount.replace(/[^0-9]/g, ''));
+	if (!isNaN(amount)) return parseInt(amount);
+	return 0;
+}
