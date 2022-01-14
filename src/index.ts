45937069c67962c0b7720d909dc94691aadf2bb0
Scott Gould <greysilly7@gmail.com>
2022-01-14 13:26:11 -0500

    Remove code that SHOULD NOT be in the REPO

--- src/index.ts
+++ src/index.ts
@@ -34,61 +34,6 @@ const startBot = async () => {
 	ApplicationCommandRegistries.setDefaultBehaviorWhenNotIdentical(RegisterBehavior.Overwrite);
 
 	connection = await createConnection(config.typeORMConfig);
-
-	const differentJobs = [
-		{
-			name: 'Janitor',
-			description: 'Mop the floor, clean the toilets.',
-			requiredXP: 0
-		},
-		{
-			name: 'Chief',
-			description: 'Cook, clean, eat.',
-			requiredXP: 300
-		},
-		{
-			name: 'Fire Fighter',
-			description: 'Fight fires, make bank.',
-			requiredXP: 450
-		},
-		{
-			name: 'YouTuber',
-			description: 'Rank in money from the ads.',
-			requiredXP: 590
-		},
-		{
-			name: 'Investor',
-			description: 'Invest in companies.',
-			requiredXP: 800
-		},
-		{
-			name: 'Pepe King',
-			description: 'You wish you were this.',
-			requiredXP: 1024
-		}
-	];
-
-	for (const job of differentJobs) {
-		let newJob = new Jobs();
-
-		newJob.name = job.name;
-		newJob.description = job.description;
-		newJob.minimumXP = job.requiredXP;
-
-		await newJob.save();
-	}
-
-	connection.getRepository(Item).manager.query(`
-    INSERT INTO economy.item (price,rarity,emoji,description,name,sellable,tradeable,collectable) VALUES
-     (800,'common',':fishing_pole_and_fish:','I was forgotten about by the devs ;(.','Fishing_Pole',0,0,0),
-     (500,'common',':scissors:','I was forgotten about by the devs ;(.','Scissors',0,0,0),
-     (5000,'rare',':tv:','I was forgotten about by the devs ;(.','TV',0,0,0),
-     (2500,'rare',':laptop:','I was forgotten about by the devs ;(.','Laptop',0,0,0),
-     (50000,'epic',':pizza:','I was forgotten about by the devs ;(.','Grilled_Cheese',0,0,0),
-     (10000,'common','<:rifle:927072788085346334>','I was forgotten about by the devs ;(.','Hunting_Rifle',0,0,0),
-     (1500,'rare',':mobile_phone:','I was forgotten about by the devs ;(.','IPhone',0,0,0),
-     (25000,'epic',':helicopter:','I was forgotten about by the devs ;(.','Helicopter',0,0,0),
-     (10000,'epic',':chicken:','I was forgotten about by the devs ;(.','Golden_Chicken_Nugget',0,0,0);`);
 };
 
 startBot();
