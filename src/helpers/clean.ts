ab4be98a81c3f8b445555c82636cbcf47f29803e
Scott Gould <greysilly7@gmail.com>
2022-01-14 17:22:12 -0500

    I forgot

--- src/helpers/clean.ts
+++ src/helpers/clean.ts
@@ -1,5 +1,7 @@
 import { config } from '../config';
-export const clean = (text: string) => {
-	const result: string = text.replace(/@everyone|@here|<@&?(\d{17,19})>/g, '<mention>').replaceAll(config.token, '');
+export const clean = (text: string): string => {
+	const result: string = text
+		.replace(/@everyone|@here|<@&?(\d{17,19})>/g, '<mention>')
+		.replaceAll(config.token, '');
 	return result;
 };
