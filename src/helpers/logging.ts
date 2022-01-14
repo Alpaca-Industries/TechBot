c98cf8341354862cd2cd8b005a605384ec215654
DevSpen <spenmusic6@gmail.com>
2022-01-13 20:06:37 -0500

    feat(logging): Improved logging

--- src/helpers/logging.ts
+++ src/helpers/logging.ts
@@ -1,5 +1,10 @@
 import { MessageEmbed } from 'discord.js';
 
-export const generateErrorEmbed = (error: string) => {
-	return new MessageEmbed().setColor('#ff0000').setTitle('Error').setDescription(error);
+export const generateErrorEmbed = (error: string, errorType: string = '') => {
+	const errType = errorType !== '' ? `: ${errorType}` : '';
+
+	return new MessageEmbed()
+		.setColor('#ED4245')
+		.setTitle('Error' + errType)
+		.setDescription(error);
 };
