483f8179484c286dfd6625640371a46d803a5a9b
DevSpen <spenmusic6@gmail.com>
2022-01-16 15:34:51 -0500

    feat(help): Better help messages

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -4,6 +4,7 @@ import { Command } from '@sapphire/framework';
 import { ApplyOptions } from '@sapphire/decorators';
 import { PaginatedMessage } from '@sapphire/discord.js-utilities';
 import { Message, MessageEmbed } from 'discord.js';
+import { getPrefix } from '../helpers/getPrefix';
 
 @ApplyOptions<CommandOptions>({
 	name: 'help',
@@ -13,22 +14,33 @@ import { Message, MessageEmbed } from 'discord.js';
 export default class helpCommand extends Command {
 	async messageRun(message: Message<boolean>, args: Args) {
 		const specifiedCommand = await args.pick('string').catch(() => '');
+		const prefix = await getPrefix(message.guild);
 		// List All Commands Registered In Sapphire
 		const commands = this.container.stores.get('commands');
-		const categories = commands.categories;
-		const paginatedMessage = new PaginatedMessage({ template: new MessageEmbed().setTitle('Help') });
 
 		if (specifiedCommand.length > 0) {
-			const command = commands.find((c) => c.name === specifiedCommand);
-			if (command === undefined) return message.reply('That command does not exist!');
-
-			paginatedMessage.addPageEmbed(
-				new MessageEmbed()
-					.setTitle(`${command.name}`)
-					.setDescription(`${command.description}`)
-					.setColor('#20ce1f')
+			const command = commands.find(
+				(c) =>
+					c.name === specifiedCommand.toLowerCase() ||
+					c.name.startsWith(specifiedCommand.toLowerCase())
 			);
+			if (!command) return message.reply('That command does not exist!');
+
+			const singleCommandResponse = new MessageEmbed()
+				.setTitle(`${command.detailedDescription}`)
+				.setDescription(`${command.description}`)
+				.setColor('BLUE');
+
+			return message.channel.send({ embeds: [singleCommandResponse] });
 		}
+
+		const categories = commands.categories;
+		const paginatedMessage = new PaginatedMessage({
+			template: new MessageEmbed()
+				.setTitle('Help')
+				.setColor('BLUE')
+				.setFooter({ text: `This server's prefix is ${prefix}` })
+		});
 
 		for (const category of categories) {
 			const fields: { name: string; value: any }[] = [];
@@ -41,15 +53,17 @@ export default class helpCommand extends Command {
 			);
 			for (const [_, command] of filteredCommands) {
 				fields.push({
-					name: `${command.name}`,
-					value: `\nDescription: ${command.description}\nUsage: ${command.detailedDescription}`
+					name: command.name,
+					value: command.description
 				});
 			}
 
 			paginatedMessage.addPageEmbed(
 				new MessageEmbed()
-					.setTitle(category)
+					.setTitle(category.toProperCase())
+					.setColor('BLUE')
 					.setDescription(fields.map((f) => `**${f.name}:** ${f.value}`).join('\n'))
+					.setFooter({ text: `This server's prefix is ${prefix}` })
 			);
 		}
 
