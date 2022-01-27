505e46897f526e820f59492559b486e30779a009
Scott Gould <greysilly7@gmail.com>
2022-01-26 22:20:48 -0500

    Fix up fish command

--- src/commands/economy/fish.ts
+++ src/commands/economy/fish.ts
@@ -3,7 +3,7 @@ import { ApplicationCommandRegistry, Args, Command, CommandOptions } from '@sapp
 
 import { CommandInteraction, Message, User } from 'discord.js';
 import { Item } from '../../entities/economy/item';
-import { fetchInventory, fetchUser } from '../../helpers/dbHelper';
+import { fetchInventory } from '../../helpers/dbHelper';
 
 @ApplyOptions<CommandOptions>({
 	name: 'fish',
@@ -12,14 +12,12 @@ import { fetchInventory, fetchUser } from '../../helpers/dbHelper';
 })
 export class StatsCommand extends Command {
 	private async fishLogic(user: User) {
-		const userdetails = await fetchUser(user);
+		const doesUserHaveFishingPole = await fetchInventory(
+			user,
+			await Item.findOne({ where: { name: 'fishing_pole' } })
+		);
 
-		if (
-			userdetails.inventory.filter(
-				async (item) => item.itemID === (await Item.findOne({ where: { name: 'fishing_pole' } })).id
-			)[0].amount === 0
-		)
-			return 'You do not have a fishing pole!';
+		if (doesUserHaveFishingPole.amount === 0) return 'You do not have a fishing pole!';
 		const fishing_success = Math.random() ? true : false;
 
 		if (fishing_success) {
