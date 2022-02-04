01e36458be3312667771cd48c5663a7ff74e41cc
Scott Gould <greysilly7@gmail.com>
2022-02-04 14:11:01 -0500

    Added money check

--- src/helpers/dbHelper.ts
+++ src/helpers/dbHelper.ts
@@ -3,6 +3,7 @@ import { Inventory } from '../entities/economy/inventory';
 import { Item } from '../entities/economy/item';
 import { User as EconomyUser } from '../entities/economy/user';
 import { Guild as DBGuild } from '../entities/guild';
+import { isSafeInteger } from './isSafeInteger';
 
 /*
 const ItemRegistry = {
@@ -24,10 +25,18 @@ export const fetchItemByName = (name: string): Promise<Item> => {
 
 export const fetchUser = async (user: DiscordUser): Promise<EconomyUser> => {
 	// Look for user if not exist make new one and return
-	const userData = await EconomyUser.findOne({ where: { id: user.id } });
+	let userData = await EconomyUser.findOne({ where: { id: user.id } });
 	if (userData === undefined) {
-		await EconomyUser.insert({ id: user.id });
-		return fetchUser(user);
+		userData = new EconomyUser();
+		userData.id = user.id;
+		userData.wallet = 0;
+		userData.bank = 0;
+		await userData.save();
+	}
+	if (!isSafeInteger(userData.wallet) || !isSafeInteger(userData.bank)) {
+		userData.wallet = 0;
+		userData.bank = 0;
+		await userData.save();
 	}
 	return userData;
 };
@@ -37,20 +46,23 @@ export const fetchInventory = async (user: DiscordUser, item: Item): Promise<Inv
 	let inventory = await Inventory.findOne({ where: { userId: userData.id, itemID: item.id } });
 	if (inventory === undefined) {
 		inventory = new Inventory();
-		inventory.userId = userData.id;
+		inventory.userId = user.id;
 		inventory.itemID = item.id;
 		inventory.amount = 0;
 		await inventory.save();
-		return fetchInventory(user, item);
 	}
 	return inventory;
 };
 
 export const fetchGuild = async (guild: DiscordGuild): Promise<DBGuild> => {
-	const guildData = await DBGuild.findOne({ where: { id: guild.id } });
+	let guildData = await DBGuild.findOne({ where: { id: guild.id } });
 	if (guildData === undefined) {
-		await DBGuild.insert({ id: guild.id, prefix: '-' });
-		return fetchGuild(guild);
+		guildData = new DBGuild();
+		guildData.id = guild.id;
+		guildData.prefix = '-';
+		guildData.slotsMoneyPool = 0;
+		guildData.slotsWinMultiplier = 0;
+		await guildData.save();
 	}
 	return guildData;
 };
